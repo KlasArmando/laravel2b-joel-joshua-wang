48d2c9a58c0c7a6fbefc54bcdc216737f4d38e26
JoelForst <suboy-joel@hotmail.com>
2018-05-30 14:23:41 +0200

    updates done by...

---
+++ database/migrations/2018_05_30_121530_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('categories_id');
+            $table->string('weather', 100);
+            $table->string('top_10', 100);
+            $table->string('ocassion', 100);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
